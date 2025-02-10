524177494dd68c6df53f1fb6787388395b5e0ef1
wandi1209 <wandider@gmail.com>
2025-02-10 18:20:12 +0700

    using PageView on apps

--- lib/screens/main_screen.dart
+++ lib/screens/main_screen.dart
@@ -9,6 +9,28 @@ class MainScreen extends StatefulWidget {
 }
 
 class _MainScreenState extends State<MainScreen> {
+  List<String> facts = [
+    "Cows have best friends and can become stressed when they are separated.",
+    "Honey never spoils. It can last for centuries without going bad.",
+    "A group of owls is called a parliament.",
+    "Bananas are berries, but strawberries aren't.",
+    "The Eiffel Tower can be 15 cm taller during the summer due to thermal expansion.",
+    "The average person laughs about 15 times a day.",
+    "A single cloud can weigh as much as 1 million pounds.",
+    "Your taste buds have a lifespan of about 10 to 14 days.",
+    "The longest recorded hiccuping lasted for 68 years!",
+    "Bees can recognize human faces.",
+    "The world's largest desert is Antarctica.",
+    "A day on Venus is longer than a year on Venus.",
+    "Butterflies taste with their feet.",
+    "The dot over the letter 'i' is called a tittle.",
+    "A group of flamingos is called a 'flamboyance.'",
+    "The shortest war in history was between Britain and Zanzibar, lasting only 38 minutes.",
+    "The smell of freshly-cut grass is actually a plant distress call.",
+    "Elephants are the only animals that can't jump.",
+    "There are more possible iterations of a game of chess than there are atoms in the observable universe.",
+    "The world's oldest toy is a stick, according to archaeological finds."
+  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -24,6 +46,31 @@ class _MainScreenState extends State<MainScreen> {
             child: Padding(
               padding: const EdgeInsets.all(8.0),
               child: Icon(Icons.settings),
+            ),
+          )
+        ],
+      ),
+      body: Column(
+        children: [
+          Expanded(
+            child: PageView.builder(
+              itemCount: facts.length,
+              itemBuilder: (BuildContext context, int index) {
+                return Container(
+                    child: Center(
+                  child: Text(
+                    facts[index],
+                    textAlign: TextAlign.center,
+                    style: TextStyle(fontSize: 30),
+                  ),
+                ));
+              },
+            ),
+          ),
+          Container(
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text("Swipe left for more -->"),
             ),
           )
         ],
